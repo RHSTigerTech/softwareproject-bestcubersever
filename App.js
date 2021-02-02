2899b21255eee2bdf4c4b2dbd579bc644b3a3db7
Nleslie2408 <54080656+nleslie2408@users.noreply.github.com>
2021-02-01 23:17:47 -0800

    changed code

--- App.js
+++ App.js
@@ -1,21 +1,40 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, 
+  Image, Alert, TouchableWithoutFeedback, 
+  TouchableOpacity, TouchableHighlight, 
+  SafeAreaView, Platform, Dimensions } from 'react-native';
+import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks'
+
+import {Button} from 'react-native-elements'
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View
+      style={{
+        backgroundColor:"#fff",
+        flex:1,
+      }}
+    >
+      <View 
+        style={{
+          backgroundColor:'dodgerblue',
+          flex: 1,
+        }}
+      />
+      <View 
+        style={{
+          backgroundColor:'gold',
+          flex: 1,
+        }}
+      />
+      <View 
+        style={{
+          backgroundColor:'tomato',
+          flex: 1,
+        }}
+      />
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
