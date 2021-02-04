a7ad3b529d42b8a127c745dc493789e3b36a26ff
[nleslie2408] <[nathaniel.leslie@student.rjuhsd.us]>
2021-02-04 11:30:10 -0800

    adding tabs

--- App.js
+++ App.js
@@ -7,19 +7,47 @@ import { StyleSheet, SafeAreaView, Text, View,
 import {useDimensions, useDeviceOrientation} from '@react-native-community/hooks'
 import GradientButton from 'react-native-gradient-buttons'
 import { Actions, Router, Scene } from "react-native-router-flux";
- 
-
+import {createAppContainer, createNavigationContainer } from "react-navigation"; 
+import { createStackNavigator} from "react-navigation-stack";
 import Constants from 'expo-constants';
 import {Button} from 'react-native-elements'
-import WelcomeScreen from './App/screens/WelcomeScreen';
-import ViewImageScreen from './App/screens/ViewImageScreen';
-import Gradient from './App/screens/Gradient';
+import {WelcomeScreen} from './App/screens/WelcomeScreen';
+import {ViewImageScreen} from './App/screens/ViewImageScreen';
+import {Gradient} from './App/screens/Gradient';
+import { render } from 'react-dom';
+import {NavigationContainer} from '@react-navigation/native';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+import {createMaterialTopTabTabNavigator} from '@react-navigation/material-top-tabs';
 
- 
-export default function App() {
-  return <Gradient/>;
+const Drawer = createDrawerNavigator();
+const Stack = createDrawerNavigator();
+const MaterialBottomTabs= createMaterialBottomTabNavigator();
+const MaterialTopTabs = createMaterialTopTabTabNavigator();
+
+export default class App extends Component {
+  render() {
+    createHomeStack = () =>
+    <Stack.Navigator>
+      <Stack.Screen name="Gradient" component={Gradient}/>
+    </Stack.Navigator>
+
   
+      return( 
+        <NavigationContainer>
+            <Drawer.Navigation>
+              <Drawer.Screen name="Home"/>
+              <Drawer.Screen name="Container" component={WelcomeScreen} />
+              <Drawer.Screen name="Favorites" component={ViewImageScreen}/>
+              <Drawer.Screen name="Settings"/>
+
+            </Drawer.Navigation>
+        </NavigationContainer>
+      );
+
+  }
+
 }
- 
  
 
