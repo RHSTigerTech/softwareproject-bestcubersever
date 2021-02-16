a499c95f3ca1a8a975ff0e0ba7ad78016cec8b6b
Nleslie2408 <54080656+nleslie2408@users.noreply.github.com>
2021-02-15 23:31:01 -0800

    added rotating cube

--- App/screens/Gradient.js
+++ App/screens/Gradient.js
@@ -4,7 +4,7 @@ import { StyleSheet, SafeAreaView, Text, View,
   Image, Platform, Dimensions, StatusBar, Pressable, opacity} from 'react-native';
 import {Header} from 'react-native-elements';
 import GradientButton from 'react-native-gradient-buttons';
-import LightVideo from '../Assets/FinalBackgroundRubiksCube.mp4';
+import LightVideo from '../Assets/RotatingCube.mp4';
 import ParticleEffectButton from 'react-particle-effect-button';
 import { Video } from 'expo-av';
 
@@ -30,6 +30,7 @@ function Gradient({navigation}) {
                 shouldPlay
                 isLooping
                 style={styles.image}
+                
                 /> 
             
           
@@ -40,7 +41,7 @@ function Gradient({navigation}) {
                     centerComponent={{ text: 'Rubiks Cube App', style: { color: 'black', fontSize:39, fontWeight:'bold'}}} 
                     />
                 
-                    <View style = {{justifyContent: 'space-evenly',flex:1, opacity:0.95}}>
+                    <View style = {{justifyContent: 'space-evenly',flex:1, opacity:0.9}}>
                         
                         <GradientButton
                             text="Learn"                        
@@ -127,10 +128,10 @@ const styles = StyleSheet.create({
     },
     image:{        
         width:'140%',
-        height:'150%',
+        height:'125%',
         position:'absolute',
-        top:-120,
-        right:-54
+        top:-80,
+        right:-69
     },
     viewStyle: {
         borderBottomColor: 'transparent',
@@ -140,7 +141,7 @@ const styles = StyleSheet.create({
     textStyle:{
         color:'black',
         fontSize:30,    
-        opacity:0.8,
+        opacity:0.9,
         
     }
     
