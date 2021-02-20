e6ff2b37aa83a574fedbc279ed4d7b3d2e5127cb
Nleslie2408 <54080656+nleslie2408@users.noreply.github.com>
2021-02-19 17:38:00 -0800

    added source

--- App/screens/VirtualCube.js
+++ App/screens/VirtualCube.js
@@ -3,6 +3,6 @@ import { WebView } from 'react-native-webview';
 
 export default class App extends React.Component {
   render() {
-    return <WebView source={require('')} />;
+    return <WebView source={{uri:'https://seanfrasure.github.io/cuber/cuber/index.html'}} />;
   }
 }
