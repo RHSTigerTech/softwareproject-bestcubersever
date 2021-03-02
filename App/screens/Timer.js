036934b22331f5957f7a212a44549f89cd224e13
Nleslie2408 <54080656+nleslie2408@users.noreply.github.com>
2021-03-01 21:57:19 -0800

    added icons that navigate to other parts of the app

--- App/screens/Timer.js
+++ App/screens/Timer.js
@@ -6,27 +6,13 @@
 
 // import React in our code
 import React, { useState, Component } from 'react';
-import PropTypes from 'prop-types';
-import GradientButton from 'react-native-gradient-buttons';
-// import all the components we are going to use
 import {SafeAreaView,StyleSheet,Text,View,TouchableHighlight,TouchableOpacity,StatusBar} from 'react-native';
 import {Header} from 'react-native-elements';
-import { TouchableWithoutFeedback } from 'react-native-gesture-handler';
-import { NavigationContainer, useNavigation } from '@react-navigation/native';
-//importing library to use Stopwatch and Timer
 import { Stopwatch, Timer } from 'react-native-stopwatch-timer';
-
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-
-import { IconButton, Colors } from 'react-native-paper';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
-const Tab = createBottomTabNavigator();
 
-
-
-  
-  
 let finaltime='';
 
 const possiblemoves = ["R", "L", "D", "U", "F", "B", "R'","L'","D'","U'","F'","B'","R2", "L2", "D2", "U2", "F2", "B2"]
@@ -84,35 +70,22 @@ function k(){
 console.log('--------------')
 
 const App = ({navigation}) => {
-  const [isTimerStart, setIsTimerStart] = useState(false);
   const [isStopwatchStart, setIsStopwatchStart] = useState(false);
-  const [timerDuration, setTimerDuration] = useState(9000);
-  const [resetTimer, setResetTimer] = useState(false);
   const [resetStopwatch, setResetStopwatch] = useState(false);
-  
-  
-  
- 
+
   return (
-    
     
     <SafeAreaView style={styles.container}>
       <View style={styles.container}>
         <View style={styles.sectionStyle}>
         
-        
-          <Stopwatch
-            
+          <Stopwatch            
             msecs
             start={isStopwatchStart}
                 
-            //To start
-            reset={resetStopwatch}
-            //To reset
             options={options}
-            //options for the styling
+
             getTime={(time) => {
-              //console.log(time);
               finaltime=time;
             }}
             
@@ -144,39 +117,67 @@ const App = ({navigation}) => {
             
             
           </TouchableOpacity>
-          {/* {Complete()} */}
-          {/* <TouchableHighlight style={styles.resetbuttonSize}
-            onPress={() => {
-              setIsStopwatchStart(false);
-              setResetStopwatch(true);
-              
-            }}>
-            <Text style={styles.resetbuttonText}>previous time</Text>
-          </TouchableHighlight> */}
+          
           <View style={{flexDirection:'row', justifyContent:'space-evenly', width:'100%'}}>
           <Icon.Button
             
-            name='chart-line'
-            
+            name='cube-outline'
+            flexDirection='column'
             backgroundColor='black'
-            //color={Colors.red500}
+            alignItems='center'
+            color='white'
+            size={30}
+            onPress={() => navigation.navigate('VirtualCube')}
+          >
+          <Text style={styles.BottomTabText}>3DCube   </Text>
+          </Icon.Button>
+          <Icon.Button
+            
+            name='chart-line'
+            alignItems='center'
+            flexDirection='column'
+            backgroundColor='black'
+            color='white'
             size={30}
             onPress={() => navigation.navigate('Statistics')}
-          />
-          <IconButton
+          >
+          <Text style={styles.BottomTabText}>Statistics   </Text>
+          </Icon.Button>
+
+          
+          <Icon.Button
             
-            icon="graph"
-            color={Colors.red500}
+            name='home'
+            flexDirection='column'
+            backgroundColor='black'
+            color='white'
             size={30}
             onPress={() => navigation.navigate('Gradient')}
-          />
-          <IconButton
+          >
+          <Text style={styles.BottomTabText}>Home   </Text>
+          </Icon.Button>
+          <Icon.Button
             
-            icon="camera"
-            color={Colors.red500}
+            name='camera'
+            flexDirection='column'
+            backgroundColor='black'
+            color='white'
             size={30}
-            onPress={() => navigation.navigate('Statistics')}
-          />
+            onPress={() => navigation.navigate('ViewImageScreen')}
+          >
+          <Text style={styles.BottomTabText}>Solver   </Text>
+          </Icon.Button>
+          <Icon.Button
+            
+            name='school'
+            flexDirection='column'
+            backgroundColor='black'
+            color='white'
+            size={30}
+            onPress={() => navigation.navigate('Learn')}
+          >
+          <Text style={styles.BottomTabText}>Learn   </Text>
+          </Icon.Button>
           </View>
         </View>
       </View>
@@ -191,15 +192,9 @@ export default App;
 
 const styles = StyleSheet.create({
 
-  viewStyle: {
-    borderBottomColor: 'transparent',
-    marginBottom:0,
-    backgroundColor:'blue'        
-  },
+  
   container: {
     flex: 1,
-    //padding: 10,
-    
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor:'black'
@@ -207,76 +202,51 @@ const styles = StyleSheet.create({
   
   sectionStyle: {
     flex: 1,
-    //marginTop: 32,
     alignItems: 'center',
     justifyContent: 'center',
   },
+
   startbuttonText: {
     fontSize: 80,
-    //marginTop: 10,
     color:'#FFF',
-    top:320,
-    
+    top:320, 
   },
-  resetbuttonText: {
-    fontSize: 40,
-    //marginTop: 10,
-    color:'#FFF',
-    position:'absolute',
-    bottom:0,
-  },
+
   ScrambleText: {
-    
     fontSize: 25,
-    //width: '70%',
     width:'60%',
-    
-    //marginTop: 10,
     color:'#FFF',
     position:'absolute',
     top:10,
     textAlign:'center',
     fontFamily: Platform.OS === 'ios' ? 'Helvetica Neue' : 'Roboto',
   },
-  resetbuttonSize:{
-      alignSelf:'stretch',
-      height: '10%',
-      alignItems:'center'
+
+  BottomTabText:{
+    fontSize:10,
+    color:'white',
+    justifyContent:'center'
   },
+
   startbuttonSize:{
-    //position:'absolute',
     width: 500,
     height: '90%',
     alignItems:'center',
   },
-  ViewContainer:{
-    //flex: 1, 
-    //alignItems: 'center', 
-    position:'absolute',
-    bottom:0,
-    
-  },
-  textStyle:{
-    color:'black',
-    fontSize:30,    
-    opacity:0.9,
-}
 
 });
 
 const options = {
   container: {
-    //flex: 1,
-    //padding: 10,
     position:'absolute',
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor:'black',
     height:70
   },
+  
   text: {
     fontSize: 70,
-    //fontFamily:'Helvetica Neue',
     fontFamily: Platform.OS === 'ios' ? 'Helvetica Neue' : 'monospace',
     color: '#FFF',
     marginLeft: 0,
