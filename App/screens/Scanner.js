96d9491e0bacc598b6791e6363c179f163397f12
Nleslie2408 <54080656+nleslie2408@users.noreply.github.com>
2021-03-03 22:59:55 -0800

    added some more variables

--- App/screens/Scanner.js
+++ App/screens/Scanner.js
@@ -2,10 +2,14 @@ import React, { useState, useEffect, useRef } from 'react';
 import { Text, View, TouchableOpacity, Image } from 'react-native';
 import { Camera } from 'expo-camera';
 import * as FileSystem from 'expo-file-system';
+
 export default function App() {
   const [hasPermission, setHasPermission] = useState(null);
   const [cameraRef, setCameraRef] = useState(null)
   const [type, setType] = useState(Camera.Constants.Type.back);
+  let PhotoUri;
+  let photo;
+  
 useEffect(() => {
     (async () => {
       const { status } = await Camera.requestPermissionsAsync();
@@ -20,40 +24,35 @@ if (hasPermission === null) {
   }
   return (
     <View style={{ flex: 1 }}>
+    
       <Camera style={{ flex: 1 }} type={type} ref={ref => {
         setCameraRef(ref) ;
   }}>
         <View
           style={{
             flex: 1,
-            backgroundColor: 'transparent',
+            //backgroundColor: 'transparent',
             justifyContent: 'flex-end'
           }}>
-          <TouchableOpacity
-            style={{
-              flex: 0.1,
-              alignSelf: 'flex-end'
-            }}
-            onPress={() => {
-              setType(
-                type === Camera.Constants.Type.back
-                  ? Camera.Constants.Type.front
-                  : Camera.Constants.Type.back
-              );
-            }}>
-            <Text style={{ fontSize: 18, marginBottom: 10, color: 'white' }}> Flip </Text>
-          </TouchableOpacity>
+
+          
           <TouchableOpacity style={{alignSelf: 'center'}} onPress={async() => {
+            photo = await cameraRef.takePictureAsync();
+            
             if(cameraRef){
-              let photo = await cameraRef.takePictureAsync();
+              //let photo = await cameraRef.takePictureAsync();
               console.log('photo', photo);
-              console.log(photo.uri)
-              FileSystem.makeDirectoryAsync('file://C:\Users\Nathan\Desktop\tempFolder')
+              
+              PhotoUri=photo.uri;
+              //FileSystem.makeDirectoryAsync('file://C:\Users\Nathan\Desktop\tempFolder')
               // FileSystem.moveAsync({
               //  from:photo.uri,
               //  to: 'C:\Users\Nathan\Desktop\temp'
               // })
+              
             }
+            console.log(PhotoUri)
+            let yourPicture = require (PhotoUri);
             
           }}>
             <View style={{ 
@@ -76,9 +75,18 @@ if (hasPermission === null) {
               </View>
             </View>
           </TouchableOpacity>
+        
           
         </View>
       </Camera>
+      
     </View>
+    
   );
+ 
 }
+
+
+  
+
+
