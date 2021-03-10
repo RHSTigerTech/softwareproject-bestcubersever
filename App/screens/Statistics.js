1881e18eb3e776bc0d8fbb2de125ba5e0082c99b
[nleslie2408] <nathaniel.leslie@student.rjuhsd.us>
2021-03-10 11:33:19 -0800

    fixed stuff

--- App/screens/Statistics.js
+++ App/screens/Statistics.js
@@ -14,19 +14,9 @@ import {
   StackedBarChart
 } from "react-native-chart-kit";
 import { View } from "react-native";
-
-
-
-// let storedTimes=times();
-
-
-
-
+import { TabRouter } from "@react-navigation/routers";
 
 const chartConfigs = [
-    
-    
-    
     
     {
       backgroundColor: "black",
@@ -37,8 +27,6 @@ const chartConfigs = [
       color: (opacity = 1) => `rgba(${255}, ${255}, ${255}, ${opacity})`
     },
     
-    
-    
   ];
 
   export default class App extends React.Component {
@@ -47,34 +35,12 @@ const chartConfigs = [
       return <StatusBar hidden />;
     }
     
-    
     render() {
-      // console.log('before')
-      // console.log(times())
-      // console.log(deletetime())
-      // console.log('end')
-      //times()
-      //try{
-      
-      //console.log(times())
-     ///
+     
     let width;
-    //let data=[0];k
+    
     let data;
-    // console.log(deletetime()[deletetime().length-1]+'----------------')
-    // console.log(times()[times().length-1]+'----------------')
-
-    // if(times()[0]===0 && times().length>1){
-    //   times().shift();
-    // }
     
-
-    
-    //console.log(data+'-------------------------')
-    // if(data[data.length-1]===data[data.length-2]){
-    //   data.pop();
-    // }
-    //console.log(data+'-------------------------')
     
     if(times()[times().length-1]!==deletetime()[deletetime().length-1] && deletetime().length>0){
       
@@ -97,33 +63,6 @@ const chartConfigs = [
         data.shift();
         }
     }
-    //console.log(data+'-------------------------')
-    
-
-    // if(deletetime[deletetime().length-1]!==deletetime()[deletetime().length-2]){
-    //   data.pop();
-    // }
-    // console.log(deletetime()[deletetime().length-1]+'---------------------------')
-    // console.log(deletetime()[deletetime().length-2]+'---------------------------')
-
-    
-
-    
-
-    // if(deletetime().length!==times().length){
-    //   data.pop();
-    // }
-    
-    
-    
-    
-    
-    
-    
-    //console.log(data.length)
-    // console.log(times())
-    // console.log(deletetime())
-    // console.log(data)
     
     //add more if else statements to male scroll look good no matter how many data points
     if (data.length>10){
@@ -132,40 +71,33 @@ const chartConfigs = [
     else{
         width = Dimensions.get("window").width;
     }
-    //width = Dimensions.get("window").width;
-    //const height='100%';
-      //const { width } = Dimensions.get("window");
-    //const {height} = Dimensions.get('window');
+
       return (
         <View style={{flex: 1, backgroundColor: 'black'}} >
         <Header                       
                     containerStyle={styles,{backgroundColor:'black', borderBottomColor:'black'}}
                     centerContainerStyle={{flex:14}}
                     centerComponent={{ text: 'Session Statistics', style: { color: 'white', fontSize:30, fontWeight:'bold', bottom:-5,backgroundColor:'transparent'}}} 
-                    //shadowColor={'transparent'}
                     />
         <ScrollView directionalLockEnabled='vertical' vertical={false} horizontal={true}  scrollEventThrottle={16} renderTabBar={this.renderTabBar}>
           {chartConfigs.map(chartConfig => {
             const labelStyle = {
               color: 'black',
               marginVertical: 0,
-              //right:-40,
               textAlign: "center",
               fontSize: 16
             };
             const graphStyle = {
-            //paddingTop: Platform.OS === 'android' ?  StatusBar.currentHeight: 0,
-            //paddingTop:20,
             strokeWidth:2,
             bottom: 0,
-            //position:'center',k
+
               ...chartConfig.style
             };
             <Text style={labelStyle}>Bezier Line Chart</Text>
             return (
               <ScrollView
                 
-                //key={Math.random()}
+                
                 style={{
                   backgroundColor: chartConfig.backgroundColor
                 }}
@@ -175,7 +107,7 @@ const chartConfigs = [
             
               <LineChart
                 
-                bezier
+                //bezier
                 data={{
                   labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
                   datasets: [
@@ -188,34 +120,28 @@ const chartConfigs = [
                 width={width}
                 height={400}
                 yAxisLabel=""
-                //yAxisSuffix=""
                 chartConfig={chartConfig}
                 
                 style={graphStyle}
                 verticalLabelRotation={0}
                 onDataPointClick={({ value, getColor }) =>
                   showMessage({
-                    position:'bottom',
-                    message: `${value}`,
-                    description: "You selected this value",
+                    //position:'bottom',
+                    message: `Times: ${value}`,
+                    //description: "You selected this value",
                     backgroundColor: 'black'
                   })
                 }
-                //formatXLabel={label => label.toUpperCase()}
+                
               />
-              <FlashMessage duration={1000} />
+              <FlashMessage duration={1000} floating={true} />
         </ScrollView>
             );
           })}
     </ScrollView>
     </View>
     );
-        
-        
-
-
-    
-    
+       
   //   }
   //   catch(err){
   //     return(
