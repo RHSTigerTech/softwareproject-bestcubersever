83f9663210cda34854ccb2a650ddab03b3e9bc40
nleslie2408 <nathaniel.leslie@student.rjuhsd.us>
2021-04-28 10:43:32 -0700

    trying to stop re-rerender on virtual cube when navigating to home screen

--- App/screens/VirtualCube.js
+++ App/screens/VirtualCube.js
@@ -1,8 +1,142 @@
-import React, {Component} from 'react';
+
+import React, { Component,useState, useRef  } from 'react';
 import { WebView } from 'react-native-webview';
+import { StyleSheet, View, Platform, StatusBar, Text, Image, Button, ScrollView, TouchableOpacity, SafeAreaView, Dimensions, ActivityIndicator} from 'react-native';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
 
 export default class App extends Component {
+  
+  shouldComponentUpdate(nextProps, nextState){
+    return !equals(nextProps, this.props); // equals() is your implementation
+ }
+
   render() {
-    return <WebView source={{uri:'https://rhstigertech.github.io/softwareproject-bestcubersever/cuber/'}} />;
+    const {navigate} = this.props.navigation;
+
+    // const myScript = 
+      
+    //   setTimeout(function() { window.alert('hi') }, 2000);
+    //   true;
+    // ;
+    return (
+      <View style={{ flex: 1 }}>
+        <View style={{height:'90%'}}>
+        <WebView
+          source={{
+            uri:
+              'https://rhstigertech.github.io/softwareproject-bestcubersever/cuber/',
+          }}
+          startInLoadingState={true}
+  renderLoading={() => (
+    <ActivityIndicator
+      color='black'
+      size='large'
+      style={styles.flexContainer}
+    />
+  )}
+          //injectedJavaScript={myScript}
+        />
+        </View>
+        <View style={{flexDirection:'row', backgroundColor:'#121212'}}>
+                
+                    <Icon.Button
+                        name='cube-outline'
+                        flexDirection='column'
+                        backgroundColor='transparent'
+                        //backgroundColor='#121212'
+                        alignItems='center'
+                        color='white'
+                        opacity={1}
+                        size={30}
+                        paddingHorizontal={Platform.OS === 'ios' ? '3%':'4.5%'}
+                        onPress={({}) => navigate('VirtualCube')}
+                        >
+                        <Text style={styles.BottomTabText}>3DCube</Text>
+                    </Icon.Button>
+                    {/* Statistics */}
+                    <Icon.Button            
+                        name='chart-line'
+                        alignItems='center'
+                        flexDirection='column'
+                        backgroundColor='transparent'
+                        //backgroundColor='#121212'
+                        color='white'
+                        opacity={1}
+                        size={30}
+                        paddingHorizontal={Platform.OS === 'ios' ? '3%':'4.5%'}
+                        onPress={() => navigate('Statistics')}
+                    >
+                    <Text style={styles.BottomTabText}>Statistics</Text>
+                    </Icon.Button>
+                    {/* Home Screen */}
+                    <Icon.Button
+                        name='home' 
+                        flexDirection='column'
+                        backgroundColor='transparent'
+                        //backgroundColor='#121212'
+                        color='white'
+                        opacity={1}
+                        size={30}
+                        paddingHorizontal={Platform.OS === 'ios' ? '3%':'4.5%'}
+                        onPress={() => navigate('Gradient')}
+                    >
+                    <Text style={styles.BottomTabText}>Home</Text>
+                    </Icon.Button>
+                    {/* Learn */}
+                    <Icon.Button
+                        name='school'
+                        backgroundColor='transparent'
+                        flexDirection='column'
+                        //backgroundColor='#121212'
+                        color='white'
+                        opacity={1}
+                        size={30}
+                        paddingHorizontal={Platform.OS === 'ios' ? '3%':'4.5%'}
+                        onPress={() => navigate('Learn')}
+                    >
+                    <Text style={styles.BottomTabText}>Learn</Text>
+                    </Icon.Button>
+                    <Icon.Button
+                        name='timer-outline'
+                        flexDirection='column'
+                        backgroundColor='transparent'
+                        //backgroundColor='#121212'
+                        alignItems='center'
+                        color='white'
+                        opacity={1}
+                        size={30}
+                        paddingHorizontal={Platform.OS === 'ios' ? '3%':'4.5%'}
+                        onPress={() => navigate('Timer')}
+                        >
+                        <Text style={styles.BottomTabText}>Timer</Text>
+                    </Icon.Button>
+                </View>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  BottomTabText:{
+    //Text used for the bottom menu
+    fontSize:10,
+    color:'white',
+    opacity:1,
+    justifyContent:'center',
+    left:'-7%',
+    paddingHorizontal:'1%'
+  },
+})
+
+// import React, {Component} from 'react';
+// import { WebView } from 'react-native-webview';
+
+// export default class App extends Component {
+//   render() {
+//     return( 
+//       <WebView source={{uri:'https://rhstigertech.github.io/softwareproject-bestcubersever/cuber/'}} />
+//     );
+//   }
+// }
