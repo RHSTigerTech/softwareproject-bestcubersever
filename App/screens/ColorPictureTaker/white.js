2cff9e46171e24c1154d0382b51fc7190ed8deae
Nleslie2408 <54080656+nleslie2408@users.noreply.github.com>
2021-05-13 23:34:33 -0700

    changed camera

--- App/screens/ColorPictureTaker/white.js
+++ App/screens/ColorPictureTaker/white.js
@@ -1,146 +1,236 @@
-import React, { Component } from 'react';
+
+
+import React, { useState, useRef, useEffect } from "react";
+import Svg, {Line} from 'react-native-svg'
 import {
-  ActivityIndicator,
-  Button,
-  Clipboard,
-  Image,
-  Share,
-  StatusBar,
   StyleSheet,
+  Dimensions,
+  View,
   Text,
   TouchableOpacity,
-  View,
-} from 'react-native';
-import { Constants } from 'expo';
-import * as Permissions from 'expo-permissions';
-import * as ImagePicker from 'expo-image-picker';
-import AwesomeButton from "react-native-really-awesome-button";
-import {val} from '../PictureTaker';
-import {Card} from 'react-native-elements';
-import {defaultCube} from '../PictureTaker'
-
-
-
-
-
-//const val = Math.floor(100000 + Math.random() * 9999999);
-
-
-let fileType= Platform.OS === 'ios' ? 'jpeg':'png'
-
-
-
-export default class App extends Component {
-  state = {
-    image: null,
-    uploading: false,
+  SafeAreaView,
+} from "react-native";
+import { Camera } from "expo-camera";
+import { Video } from "expo-av";
+const WINDOW_HEIGHT = Dimensions.get("window").height;
+const WINDOW_WIDTH = Dimensions.get("window").width;
+const closeButtonSize = Math.floor(WINDOW_HEIGHT * 0.032);
+const captureSize = Math.floor(WINDOW_HEIGHT * 0.09);
+export default function App() {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [cameraType, setCameraType] = useState(Camera.Constants.Type.back);
+  const [isPreview, setIsPreview] = useState(false);
+  const [isCameraReady, setIsCameraReady] = useState(false);
+  const [isVideoRecording, setIsVideoRecording] = useState(false);
+  const [videoSource, setVideoSource] = useState(null);
+  const cameraRef = useRef();
+  useEffect(() => {
+    (async () => {
+      const { status } = await Camera.requestPermissionsAsync();
+      setHasPermission(status === "granted");
+    })();
+  }, []);
+  const onCameraReady = () => {
+    setIsCameraReady(true);
   };
-//   UNSAFE_componentWillMount() { 
-//     this._takePhoto()
-// }
-  
-  render() {
-    const {navigate} = this.props.navigation;
-
-    let {
-      image
-    } = this.state;
-    
-    console.log(val);
-    return (
-      <View style={styles.container}>
-         
-         <View style={{flex:1, top:'3%'}}>
-         <Card containerStyle={{backgroundColor:'#121212'}}>
-        <Text style={{color:'white', fontWeight:'bold', textAlign:'center', fontSize:30, paddingBottom:'5%', paddingHorizontal:'10%'}}>Take A Photo Of The White Center With The Blue Center Facing You.</Text>
-        </Card>
-
-        </View>
-      <View style={{flex:1,justifyContent:'flex-end',bottom:'10%' ,}}>
-        <AwesomeButton 
-            width={300} 
-            height={65}
-            backgroundColor='#6d00eb'
-            textSize={27}
-            borderRadius={10}
-            activeOpacity={.8}	
-            backgroundDarker='#5c00c7'
-            backgroundShadow='transparent'
-            raiseLevel={5}
-            onPress={this._takePhoto}
-            >
-            Take Photo
-        </AwesomeButton>
-        </View>
-      </View>
-      
-    );
-  }
-
-  _takePhoto = async () => {
-    const { navigate } = this.props.navigation;
-
-    const {
-      status: cameraPerm
-    } = await Permissions.askAsync(Permissions.CAMERA);
-
-    
-
-    // only if user allows permission to camera AND camera roll
-    if (cameraPerm === 'granted') {
-      let pickerResult = await ImagePicker.launchCameraAsync({
-        allowsEditing: true,
-        aspect: [4, 3],
-      });
-
-      if (!pickerResult.cancelled) {
-        this.setState({ image: pickerResult.uri });
-        this.uploadImageAsync(pickerResult.uri);
-        navigate('Blue')
-      }      
+  const takePicture = async () => {
+    if (cameraRef.current) {
+      const options = { quality: 0.5, base64: true, skipProcessing: true };
+      const data = await cameraRef.current.takePictureAsync(options);
+      const source = data.uri;
+      if (source) {
+        await cameraRef.current.pausePreview();
+        setIsPreview(true);
+        console.log("picture source", source);
+      }
     }
   };
-
- uploadImageAsync(pictureuri) {
-  let apiUrl = 'https://metal-density-310218.wl.r.appspot.com/endpoint';
-  
-  console.log('white'+val+'.'+fileType)
-
-    var data = new FormData();  
-    data.append('file', {  
-      uri: pictureuri,
-      name: 'white'+val+'.'+fileType,
-      type: 'image/'+fileType
-      
-    })
-    
-    fetch(apiUrl, {  
-      headers: {
-        'Accept': 'application/json',
-        'Content-Type': 'multipart/form-data'
-      },
-      method: 'POST',
-      body: data
-    }).then(
-      response => {
-        console.log('succ ')
-        console.log(response)
+  const recordVideo = async () => {
+    if (cameraRef.current) {
+      try {
+        const videoRecordPromise = cameraRef.current.recordAsync();
+        if (videoRecordPromise) {
+          setIsVideoRecording(true);
+          const data = await videoRecordPromise;
+          const source = data.uri;
+          if (source) {
+            setIsPreview(true);
+            console.log("video source", source);
+            setVideoSource(source);
+          }
+        }
+      } catch (error) {
+        console.warn(error);
       }
-      ).catch(err => {
-      console.log('err ')
-      console.log(err)
-      amount--;
-    } )
-    
+    }
+  };
+  const stopVideoRecording = () => {
+    if (cameraRef.current) {
+      setIsPreview(false);
+      setIsVideoRecording(false);
+      cameraRef.current.stopRecording();
+    }
+  };
+  const switchCamera = () => {
+    if (isPreview) {
+      return;
+    }
+    setCameraType((prevCameraType) =>
+      prevCameraType === Camera.Constants.Type.back
+        ? Camera.Constants.Type.front
+        : Camera.Constants.Type.back
+    );
+  };
+  const cancelPreview = async () => {
+    await cameraRef.current.resumePreview();
+    setIsPreview(false);
+    setVideoSource(null);
+  };
+  const renderCancelPreviewButton = () => (
+    <TouchableOpacity onPress={cancelPreview} style={styles.closeButton}>
+      <View style={[styles.closeCross, { transform: [{ rotate: "45deg" }] }]} />
+      <View
+        style={[styles.closeCross, { transform: [{ rotate: "-45deg" }] }]}
+      />
+    </TouchableOpacity>
+  );
+  const renderVideoPlayer = () => (
+    <Video
+      source={{ uri: videoSource }}
+      shouldPlay={true}
+      style={styles.media}
+    />
+  );
+  const renderVideoRecordIndicator = () => (
+    <View style={styles.recordIndicatorContainer}>
+      <View style={styles.recordDot} />
+      <Text style={styles.recordTitle}>{"Recording..."}</Text>
+    </View>
+  );
+  const renderCaptureControl = () => (
+    <View style={styles.control}>
+      <TouchableOpacity disabled={!isCameraReady} onPress={switchCamera}>
+        <Text style={styles.text}>{"Flip"}</Text>
+      </TouchableOpacity>
+      <TouchableOpacity
+        activeOpacity={0.7}
+        disabled={!isCameraReady}
+        onLongPress={recordVideo}
+        onPressOut={stopVideoRecording}
+        onPress={takePicture}
+        style={styles.capture}
+      />
+      
+    </View>
+  );
+
+  const renderGrid = () => (
+    <Svg height="100%" width="100%">
+  <Line x1="12.5%" y1="25%" x2="12.5%" y2="70%" stroke="white" strokeWidth="2" />
+  <Line x1="37.5%" y1="25%" x2="37.5%" y2="70%" stroke="white" strokeWidth="2" />
+  <Line x1="62.5%" y1="25%" x2="62.5%" y2="70%" stroke="white" strokeWidth="2" />
+  <Line x1="87.5%" y1="25%" x2="87.5%" y2="70%" stroke="white" strokeWidth="2" />
+
+
+  <Line x1="12.5%" y1="25%" x2="87.5%" y2="25%" stroke="white" strokeWidth="2" />
+  <Line x1="12.5%" y1="40%" x2="87.5%" y2="40%" stroke="white" strokeWidth="2" />
+  <Line x1="12.5%" y1="55%" x2="87.5%" y2="55%" stroke="white" strokeWidth="2" />
+  <Line x1="12.5%" y1="70%" x2="87.5%" y2="70%" stroke="white" strokeWidth="2" />
+</Svg>
+  );
+
+  if (hasPermission === null) {
+    return <View />;
   }
-
+  if (hasPermission === false) {
+    return <Text style={styles.text}>No access to camera</Text>;
+  }
+  return (
+    <SafeAreaView style={styles.container}>
+      <Camera
+        ref={cameraRef}
+        style={styles.container}
+        type={cameraType}
+        flashMode={Camera.Constants.FlashMode.on}
+        onCameraReady={onCameraReady}
+        onMountError={(error) => {
+          console.log("cammera error", error);
+        }}
+      />
+      <View style={styles.container}>
+        {isVideoRecording && renderVideoRecordIndicator()}
+        {videoSource && renderVideoPlayer()}
+        {isPreview && renderCancelPreviewButton()}
+        {!videoSource && !isPreview && renderCaptureControl()}
+        {renderGrid()}
+      </View>
+    </SafeAreaView>
+  );
 }
-
 const styles = StyleSheet.create({
   container: {
-    alignItems: 'center',
-    flex: 1,
-    //justifyContent: 'center',
-    backgroundColor:'#121212'
+    ...StyleSheet.absoluteFillObject,
+  },
+  closeButton: {
+    position: "absolute",
+    top: 35,
+    left: 15,
+    height: closeButtonSize,
+    width: closeButtonSize,
+    borderRadius: Math.floor(closeButtonSize / 2),
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#c4c5c4",
+    opacity: 0.7,
+    zIndex: 2,
+  },
+  media: {
+    ...StyleSheet.absoluteFillObject,
+  },
+  closeCross: {
+    width: "68%",
+    height: 1,
+    backgroundColor: "black",
+  },
+  control: {
+    position: "absolute",
+    flexDirection: "row",
+    bottom: 38,
+    width: "100%",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  capture: {
+    backgroundColor: "#f5f6f5",
+    borderRadius: 5,
+    height: captureSize,
+    width: captureSize,
+    borderRadius: Math.floor(captureSize / 2),
+    marginHorizontal: 31,
+  },
+  recordIndicatorContainer: {
+    flexDirection: "row",
+    position: "absolute",
+    top: 25,
+    alignSelf: "center",
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "transparent",
+    opacity: 0.7,
+  },
+  recordTitle: {
+    fontSize: 14,
+    color: "#ffffff",
+    textAlign: "center",
+  },
+  recordDot: {
+    borderRadius: 3,
+    height: 6,
+    width: 6,
+    backgroundColor: "#ff0000",
+    marginHorizontal: 5,
+  },
+  text: {
+    color: "#fff",
   },
 });
