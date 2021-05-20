7bec9cde57990598210d8bc5ce6888acc23a27bc
nleslie2408 <nathaniel.leslie@student.rjuhsd.us>
2021-05-20 08:46:49 -0700

    added bottom tab and tested new solution

--- App/screens/Solution.js
+++ App/screens/Solution.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import {
   ActivityIndicator,
   Button,
@@ -760,7 +761,7 @@ let sectionData=[]
     function renderfinalSolution() {
       let num;
       for( let i=0; i<finalSolution.length;i++){
-        num=i;
+        num=i+1;
         
         if(finalSolution[i]=='R'){
           
@@ -1260,6 +1261,85 @@ let sectionData=[]
   
   </View>
   </View>
+  <View style={{backgroundColor:'#121212', flexDirection:'row', alignItems:'center', justifyContent:'space-evenly', left:'1.35%'}}>
+                 {/* Home Screen */}
+                 <Icon.Button
+                        name='home' 
+                        flexDirection='column'
+                        backgroundColor='transparent'
+                        //backgroundColor='#121212'
+                        color='white'
+                        alignSelf='center'
+                        opacity={1}
+                        size={30}
+                        paddingHorizontal='4%'
+                        onPress={() => navigate('Gradient')}
+                    >
+                    <Text style={styles.BottomTabText}>Home</Text>
+                    </Icon.Button>
+                    <Icon.Button
+                        name='school'
+                        backgroundColor='transparent'
+                        flexDirection='column'
+                        //backgroundColor='#121212'
+                        color='white'
+                        opacity={1}
+                        size={30}
+                        alignSelf='center'
+                        paddingHorizontal='4%'
+                        onPress={() => navigate('Learn')}
+                    >
+                    <Text style={styles.BottomTabText}>Learn</Text>
+                    </Icon.Button>
+
+                    <Icon.Button
+                        name='timer-outline'
+                        flexDirection='column'
+                        backgroundColor='transparent'
+                        //backgroundColor='#121212'
+                        alignSelf='center'
+                        color='white'
+                        opacity={1}
+                        size={30}
+                        paddingHorizontal='4%'
+                        onPress={() => navigate('Timer')}
+                        >
+                        <Text style={styles.BottomTabText}>Timer</Text>
+                    </Icon.Button>
+                    {/* Learn */}
+                    
+                    
+            <Icon.Button
+                        name='cube-outline'
+                        flexDirection='column'
+                        backgroundColor='transparent'
+                        //backgroundColor='#121212'
+                        alignSelf='center'
+                        color='white'
+                        opacity={1}
+                        size={30}
+                        paddingHorizontal='4%'
+                        onPress={({}) => navigate('VirtualCube')}
+                        >
+                        <Text style={styles.BottomTabText}>3DCube</Text>
+                    </Icon.Button>
+                    {/* Statistics */}
+                    <Icon.Button            
+                        name='chart-line'
+                        alignSelf='center'
+                        flexDirection='column'
+                        backgroundColor='transparent'
+                        //backgroundColor='#121212'
+                        color='white'
+                        opacity={1}
+                        size={30}
+                        paddingHorizontal='4%'
+                        onPress={() => navigate('Statistics')}
+                    >
+                    <Text style={styles.BottomTabText}>Statistics</Text>
+                    </Icon.Button>
+                   
+            </View>
   </SafeAreaView>
     );
   }
@@ -1316,5 +1396,14 @@ ContentsTitle: {
   fontSize: 25,
   fontWeight:'bold',
   
+},
+BottomTabText: {
+  //Text used for the bottom menu
+  fontSize: 10,
+  color: 'white',
+  opacity: 1,
+  justifyContent: 'center',
+  left: '-7%',
+  paddingHorizontal: '1%'
 },
   });
